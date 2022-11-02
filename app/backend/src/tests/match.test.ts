132517857f0e67df825c531178a05b760ce53583
Martin Bachmann <martinpezbach@gmail.com>
2022-11-02 11:29:48 -0300

    test: add tests for get matches in progress

--- app/backend/src/tests/match.test.ts
+++ app/backend/src/tests/match.test.ts
@@ -4,13 +4,28 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 import { app } from '../app';
 import { Model } from 'sequelize';
-import { allMatchesReturn } from './mocks/match.mock';
+import { allMatchesReturn, matchesInProgressReturn } from './mocks/match.mock';
 
 chai.use(chaiHttp)
 
 const { expect } = chai
 
 describe('GET /matches', () => {
+  describe('se estiver com a query inProgress como true', () => {
+    beforeEach(() => {
+      sinon.stub(Model, 'findAll').resolves(matchesInProgressReturn as any)
+    })
+    afterEach(() => sinon.restore())
+
+    it('deve retornar um status 200', async () => {
+      const httpResponse = await chai
+        .request(app)
+        .get('/matches?inProgress=true')
+
+        expect(httpResponse.status).to.equal(200)
+        expect(httpResponse.body).to.deep.equal(matchesInProgressReturn)
+    })
+  })
   describe('em caso de sucesso', () => {
     beforeEach(() => {
       sinon.stub(Model, 'findAll').resolves(allMatchesReturn as any)
--- app/backend/src/tests/mocks/match.mock.ts
+++ app/backend/src/tests/mocks/match.mock.ts
@@ -28,3 +28,34 @@ export const allMatchesReturn = [
     }
   }
 ]
+
+export const matchesInProgressReturn = [
+  {
+    "id": 41,
+    "homeTeam": 16,
+    "homeTeamGoals": 2,
+    "awayTeam": 9,
+    "awayTeamGoals": 0,
+    "inProgress": true,
+    "teamHome": {
+      "teamName": "São Paulo"
+    },
+    "teamAway": {
+      "teamName": "Internacional"
+    }
+  },
+  {
+    "id": 42,
+    "homeTeam": 6,
+    "homeTeamGoals": 1,
+    "awayTeam": 1,
+    "awayTeamGoals": 0,
+    "inProgress": true,
+    "teamHome": {
+      "teamName": "Ferroviária"
+    },
+    "teamAway": {
+      "teamName": "Avaí/Kindermann"
+    }
+  }
+]
